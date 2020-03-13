1ef21b814ad50f09ea02259588957fdcce4aa9af
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-13 18:01:31 +0800

    - Add ScreenOrientation - Done dice animation frames - Add bottle mode - Setup mode toggle [ Incomplete ]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,11 +16,12 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ScreenOrientation,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +10,153 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  dot1: any;
+  dot2: any;
+  dot3: any;
+  dot4: any;
+  dot5: any;
+  dot6: any;
+  diceMode: any;
+  bottleMode: any;
+  mode: any;
+  public show: boolean = true;
+
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    // private screenOrientation: ScreenOrientation, 
+    private renderer: Renderer) { }
 
   ngOnInit() {
+    // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+    this.dot1 = document.querySelector('#dot1');
+    this.dot2 = document.querySelector('#dot2');
+    this.dot3 = document.querySelector('#dot3');
+    this.dot4 = document.querySelector('#dot4');
+    this.dot5 = document.querySelector('#dot5');
+    this.dot6 = document.querySelector('#dot6');
+    this.mode = document.querySelector("#mode");
+    this.diceMode = document.querySelector("#diceMode");
+    this.bottleMode = document.querySelector("#bottleMode");
+
+    this.changeMode();
+    this.setTransitions();
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
+  setTransitions() {
+    this.renderer.setElementStyle(this.dot1, 'transition', '0.1s linear');
+    this.renderer.setElementStyle(this.dot2, 'transition', '0.1s linear');
+    this.renderer.setElementStyle(this.dot3, 'transition', '0.1s linear');
+    this.renderer.setElementStyle(this.dot4, 'transition', '0.1s linear');
+    this.renderer.setElementStyle(this.dot5, 'transition', '0.1s linear');
+    this.renderer.setElementStyle(this.dot6, 'transition', '0.1s linear');
+  }
+
+  setOne(){
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot2, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot3, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot4, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot5, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot6, 'transform','scale(1) translate(1, 1)');
+    
+    this.renderer.setElementStyle(this.dot1, 'opacity', '1');
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1.5) translate(-180, 58)');
+    this.renderer.setElementStyle(this.dot2, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot3, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot4, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot5, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot6, 'opacity', '0');
+  }
+
+  setTwo(){
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1.2) translate(-55, 25)');
+    this.renderer.setElementAttribute(this.dot2, 'transform','scale(1.2) translate(-25, 186)');
+    this.renderer.setElementAttribute(this.dot3, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot4, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot5, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot6, 'transform','scale(1) translate(1, 1)');
+    
+    this.renderer.setElementStyle(this.dot2, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot3, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot4, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot5, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot6, 'opacity', '0');
+  }
+  
+  setThree(){
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1.1) translate(-20, 10)');
+    this.renderer.setElementAttribute(this.dot2, 'transform','scale(1.1) translate(-30, 226)');
+    this.renderer.setElementAttribute(this.dot3, 'transform','scale(1.1) translate(-120, -24)');
+    this.renderer.setElementAttribute(this.dot4, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot5, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot6, 'transform','scale(1) translate(1, 1)');
+    
+    this.renderer.setElementStyle(this.dot1, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot2, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot3, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot4, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot5, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot6, 'opacity', '0');
+  }
+  
+  setFour(){
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1) translate(1, 50)');
+    this.renderer.setElementAttribute(this.dot2, 'transform','scale(1) translate(1, 50)');
+    this.renderer.setElementAttribute(this.dot3, 'transform','scale(1) translate(1, 90)');
+    this.renderer.setElementAttribute(this.dot4, 'transform','scale(1) translate(1, 90)');
+    this.renderer.setElementAttribute(this.dot5, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot6, 'transform','scale(1) translate(1, 1)');
+    
+    this.renderer.setElementStyle(this.dot1, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot2, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot3, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot4, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot5, 'opacity', '0');
+    this.renderer.setElementStyle(this.dot6, 'opacity', '0');
+  }
+  
+  setFive(){
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1) translate(16, 35)');
+    this.renderer.setElementAttribute(this.dot2, 'transform','scale(1) translate(-14, 35)');
+    this.renderer.setElementAttribute(this.dot3, 'transform','scale(1) translate(16, 105)');
+    this.renderer.setElementAttribute(this.dot4, 'transform','scale(1) translate(-14, 105)');
+    this.renderer.setElementAttribute(this.dot5, 'transform','scale(1) translate(-95, -142)');
+    this.renderer.setElementAttribute(this.dot6, 'transform','scale(1) translate(1, 1)');
+
+    this.renderer.setElementStyle(this.dot1, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot2, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot3, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot4, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot5, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot6, 'opacity', '0');
+  }
+  
+  setSix(){
+    this.renderer.setElementAttribute(this.dot1, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot2, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot3, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot4, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot5, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementAttribute(this.dot6, 'transform','scale(1) translate(1, 1)');
+    this.renderer.setElementStyle(this.dot1, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot1, 'tranform', 'translate(1,1)');
+    this.renderer.setElementStyle(this.dot2, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot3, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot4, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot5, 'opacity', '1');
+    this.renderer.setElementStyle(this.dot6, 'opacity', '1');
+  }
+
+  changeMode() {
+    let bool = this.mode.checked;
+    if (bool) {
+      this.renderer.setElementStyle(this.diceMode, 'display', 'block');
+      this.renderer.setElementStyle(this.bottleMode, 'display', 'none');
+    }
+    else {
+      this.renderer.setElementStyle(this.diceMode, 'display', 'none');
+      this.renderer.setElementStyle(this.bottleMode, 'display', 'block');
+    }
+  }
 }
